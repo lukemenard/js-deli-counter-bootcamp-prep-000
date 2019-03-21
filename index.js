76bb97e3396f70e224b8aae6031026901c808cc2
Luke Menard <lukemenard@gmail.com>
2019-03-21 22:50:39 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -23,7 +23,7 @@ var line = []
 function currentLine(line){
   let i = 0
   while (i < katzDeli.length){
-    line.push(` ` + [i+1] + `. ` + katzDeli[i])
+    line.push(` [i+1] + `. ` + katzDeli[i])
     i++
   }
   if (katzDeli.length === 0){
