3c662235ed655ee03a5e59eb59ed64f87b55cea7
Luke Menard <lukemenard@gmail.com>
2019-03-21 22:56:42 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -23,10 +23,10 @@ function currentLine(katzDeli){
   var line = []
   let i = 0
   for (i < katzDeli.length){
-    line.push(`${i + 1}. ${line[i]}`)
+    line.push(`${i + 1}. ${katzDeli[i]}`)
     i++
   }
-  if (line.length === 0){
+  if (katzDeli.length === 0){
     return "The line is currently empty."
   }
   else {
